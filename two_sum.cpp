2962f897aad77a7c5d3549aa5a67472f529abde3
leowu <wuzonghua82@gmail.com>
2024-09-10 11:08:11 +0800

    first commit

---
+++ two_sum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (/* TODO */) {
+                // found pair
+                return {i, j};
+            }
+        }
+    }   
+    return {};
+}
+
+int main() {
+    vector<int> v = {2,4,7,9,11,19};
+    vector<int> ans = twoSum(v, 15);
+    cout<< "The indices of the two numbers that add up to target: " << ans[0] << ", " << ans[1];
+}
