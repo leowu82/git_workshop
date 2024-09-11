0cc54680ff2eb203e0213147a425b3dbac1f9564
071730 <ritachi21@gmail.com>
2024-09-11 20:54:09 +0800

    o(n^2) solution

--- two_sum.cpp
+++ two_sum.cpp
@@ -3,21 +3,26 @@
 
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
+vector<int> twoSum(vector<int> &nums, int target)
+{
     int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (/* TODO */) {
-                // found pair
-                return {i, j};
-            }
-        }
-    }   
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
+    {
+        if (nums[i] + nums[j] == target)
+            return {i, j};
+        else if (nums[i] + nums[j] > target)
+            j--;
+        else
+            i++;
+    }
     return {};
 }
 
-int main() {
-    vector<int> v = {2,4,7,9,11,19};
+int main()
+{
+    vector<int> v = {2, 4, 7, 9, 11, 19};
     vector<int> ans = twoSum(v, 15);
-    cout<< "The indices of the two numbers that add up to target: " << ans[0] << ", " << ans[1];
+    cout << "The indices of the two numbers that add up to target: " << ans[0] << ", " << ans[1];
 }
