c431b8f06b9aabfebf621849cf26a0b4f6b65244
Kyle9410-Chen <kyle941010@gmail.com>
2024-09-11 20:00:06 +0800

    o(n^2) solution

--- two_sum.cpp
+++ two_sum.cpp
@@ -7,7 +7,7 @@ vector<int> twoSum(vector<int>& nums, int target) {
     int n = nums.size();
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (/* TODO */) {
+            if (nums[i] + nums[j] == target) {
                 // found pair
                 return {i, j};
             }
