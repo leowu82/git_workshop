5a76f5a1906aab48188ff35f357964fd9dc7c336
haha547 <howard19970613@gmail.com>
2024-09-11 20:07:32 +0800

    nlogn

--- two_sum.cpp
+++ two_sum.cpp
@@ -5,10 +5,9 @@ using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
     int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (/* TODO */) {
-                // found pair
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i; j < n; j++) {
+            if (nums[i]+nums[j] == target) {
                 return {i, j};
             }
         }
